a5284cc53e67d463d367b494de933eeb6bff4a53
Adam Hunter <hunteradam13@gmail.com>
2021-10-25 15:43:11 -0600

    adding another test

--- project.js
+++ project.js
@@ -2,6 +2,9 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
-
+function getColor(color){
+    return `My favorite color is ${color}`;
+}
 
 module.exports = getGreeting;
+module.exports = getColor;
