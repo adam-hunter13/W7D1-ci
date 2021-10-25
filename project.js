5cf467ba68431480008942ec07d51145435c2182
Adam Hunter <hunteradam13@gmail.com>
2021-10-25 16:30:11 -0600

    another one

--- project.js
+++ project.js
@@ -2,9 +2,9 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
-function getColor(color){
-    return `My favorite color is ${color}`;
-}
+// function getColor(color){
+//     return `My favorite color is ${color}`;
+// }
 
 module.exports = getGreeting;
-module.exports = getColor;
+// module.exports = getColor;
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const getGreeting = require('./project');
-const getColor = require('./project');
+// const getColor = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
 
-it('state favorite color', () => {
-  expect(getColor('blue')).toBe("My favorite color is blue");
-});
+// test('state favorite color', () => {
+//   expect(getColor('blue')).toBe("My favorite color is blue");
+// });
